c4ec652d4ec512088c88e56699ed520cb409e5ee
jacksleath <jackwilliamsleath@gmail.com>
2023-05-26 23:17:02 +0100

    emoji helper and tests added, needs work

--- GolfScore.Services/Helpers/EmojiString.cs
+++ GolfScore.Services/Helpers/EmojiString.cs
@@ -4,10 +4,17 @@ namespace GolfScore.Services.Helpers
 {
     public static class EmojiString
     {
-        private static readonly Regex emojiChecker = new Regex("(\u00a9|\u00ae|[\u2000-\u3300] |\ud83c[\ud000 -\udfff] |\ud83d[\ud000 -\udfff] |\ud83e[\ud000-\udfff])");
+        private static readonly Regex emojiChecker = new Regex("(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])");
         public static bool ValidProfile(string textToCheck)
         {
-            return emojiChecker.IsMatch(textToCheck) && textToCheck.Length == 1;
+            //TODO: also check if its just 1 emoji
+            return emojiChecker.IsMatch(textToCheck);
         }
+
+        //public static int HowManyMatches(string textToCheck)
+        //{
+        //    var matches = emojiChecker.Matches(textToCheck);
+        //    return matches.Count();
+        //}
     }
 }
