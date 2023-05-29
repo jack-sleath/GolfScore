fd29bbfb978ec0a8dbe7d6ec86ced009bda67499
jacksleath <jackwilliamsleath@gmail.com>
2023-05-29 20:06:31 +0100

    DI for CosmosDB

--- GolfScore.API/Program.cs
+++ GolfScore.API/Program.cs
@@ -2,6 +2,8 @@ using GolfScore.Repositories;
 using GolfScore.Repositories.Interfaces;
 using GolfScore.Services;
 using GolfScore.Services.Interfaces;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient();
+
+string cosmosDbConnection = builder.Configuration["CosmosDBConnection"];
+builder.Services.AddSingleton<CosmosClient>(serviceProvider =>
+{
+    IHttpClientFactory httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+
+    CosmosClientOptions cosmosClientOptions = new CosmosClientOptions
+    {
+        HttpClientFactory = httpClientFactory.CreateClient,
+        ConnectionMode = ConnectionMode.Gateway
+    };
+
+    return new CosmosClient(cosmosDbConnection, cosmosClientOptions);
+});
 
 builder.Services.AddScoped<IPlayerService, PlayerService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
--- GolfScore.Models/Course.cs
+++ GolfScore.Models/Course.cs
@@ -2,17 +2,27 @@
 {
     public class Course
     {
-        public string ExternalID { get; set; }
-        public string Name { get; set; }
-        public Location? Coordinates { get; set; }
-        public bool IsFictional { get; set; }
-        public bool IsVerified { get; set; }
-        public List<Hole> Holes { get; set; } = new List<Hole>();
+        public string? ExternalId { get; private set; }
+        public string Name { get; private set; }
+        public Location? Coordinates { get; private set; } = null;
+        public bool IsFictional { get; private set; }
+        public bool IsVerified { get; private set; } = false;
+        public List<Hole> Holes { get; private set; } = new List<Hole>();
         public Dictionary<Guid, DateTime> Editors { get; private set; } = new Dictionary<Guid, DateTime>();
+        public Course(string name, bool isFictional, string externalId = null)
+        {
+            Name = name;
+            ExternalId = externalId;
+            IsFictional = isFictional;
+        }
         public void AddEditor(Guid PlayerId, DateTime editDateTime)
         {
             Editors.Add(PlayerId, editDateTime);
         }
 
+        public void VerifyCourse()
+        {
+            IsVerified = true;
+        }
     }
 }
